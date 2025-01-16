72c9db864ed93fecbc01bea587ff0830d278b3ce
Pankaj saini <96692652+Pankaj21bm@users.noreply.github.com>
2025-01-16 19:50:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    // <BrowserRouter>
     <Routes>
       <Route path="/" element={<CryptoDashBoard crypto="bitcoin" />} />
       <Route path="/bitcoin" element={<CryptoDashBoard crypto="bitcoin" />} />
@@ -16,7 +16,7 @@ const App = () => {
       <Route path="/litecoin" element={<CryptoDashBoard crypto="litecoin" />} />
       <Route path="/dogecoin" element={<CryptoDashBoard crypto="dogecoin" />} /> 
     </Routes>
-     </BrowserRouter>
+     // </BrowserRouter>
   );
 };
 
